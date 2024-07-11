f94e6e318d0b1e3ed956d513b79af410b1b2c241
Suan Choi <ziki114455@gmail.com>
2024-07-11 21:21:38 +0900

    키워드검색

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 let newsList = [];
+let url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`) // 전역변수로 빼줌
+
+const searchInput = document.getElementById("search-input")
 const menus = document.querySelectorAll(".menus button");
+const getNews = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    newsList = data.articles;
+    render(); 
+}
+
 menus.forEach((menu) =>
   menu.addEventListener("click", (event) => getNewsByCategory(event))
 );
 
+searchInput.addEventListener("keydown", (event)=>{
+    if(event.key === "Enter") {
+        getNewsByKeyword(event)
+    }
+});
+
 // API 호출
 const getLatestNews = async () => {
-  const url = new URL(
+  url = new URL(
     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`
   );
-  const response = await fetch(url);
-  const data = await response.json();
-  newsList = data.articles;
-  render(); // 뉴스리스트가 확정되면 바로 함수 실행
-  console.log("d", newsList);
+  getNews(); // 리펙토링
 };
 getLatestNews();
 
@@ -51,8 +64,7 @@ const render = () => {
     <div class="col-lg-8 right">
         <h2>${news.title}</h2>
         <p>
-        ${
-          news.description == null || news.description == ""
+        ${news.description == null || news.description == ""
             ? "내용없음"
             : news.description.length >= 200
             ? news.description.substring(0, 200) + "..." // slice()와 가능 같음
@@ -73,25 +85,19 @@ const render = () => {
 const getNewsByCategory = async (event) => {
   const category = event.target.textContent.toLowerCase(); // 소문자로 변경
   console.log("category", category);
-  const url = new URL(
+  url = new URL(
     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}`
   );
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log("dd", data);
-  newsList = data.articles; // 뉴스리스트에 내용을 넣어주어야 render함수 실행 가능
-  render();
+  getNews();
 };
 
 // 키워드별 검색 함수
 const getNewsByKeyword = async () => {
-  const keyword = document.getElementById("search-input").value;
-  const url = new URL(
+  const keyword = searchInput.value;
+  console.log("keyword", keyword);
+  url = new URL(
     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&q=${keyword}`
   );
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log("ddd", data);
-  newsList = data.articles;
-  render();
+  searchInput.value = "";
+  getNews();
 };
