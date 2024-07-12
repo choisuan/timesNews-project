c75423bfaca73bb84ea5ee34ec057fb11c033789
Suan Choi <ziki114455@gmail.com>
2024-07-12 17:30:40 +0900

    에러핸들링까지

--- main.js
+++ main.js
@@ -1,73 +1,60 @@
 let newsList = [];
 let url = new URL(
-    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`) // 전역변수로 빼줌
-
-const searchInput = document.getElementById("search-input")
-const menus = document.querySelectorAll(".menus button");
-const getNews = async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    newsList = data.articles;
-    render(); 
-}
-
-menus.forEach((menu) =>
-  menu.addEventListener("click", (event) => getNewsByCategory(event))
+  `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`
 );
 
-searchInput.addEventListener("keydown", (event)=>{
-    if(event.key === "Enter") {
-        getNewsByKeyword(event)
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+const menus = document.querySelectorAll(".menus button");
+const defaultImage =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU";
+
+// 뉴스 API 호출
+const getNews = async () => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (response.status === 200) {
+      if (data.articles.length === 0) {
+        throw new Error("뉴스 결과가 없습니다.");
+      }
+      newsList = data.articles;
+      render();
+    } else {
+      throw new Error(data.message);
     }
-});
+  } catch (error) {
+    errorRender(error.message);
+  }
+};
 
 // API 호출
 const getLatestNews = async () => {
   url = new URL(
     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`
   );
-  getNews(); // 리펙토링
+  await getNews(); // 리펙토링
 };
 getLatestNews();
 
-// 사이드바 열기 함수
-const openNav = () => {
-  document.getElementById("mySidenav").style.width = "250px";
-};
-
-// 사이드바 닫기 함수
-const closeNav = () => {
-  document.getElementById("mySidenav").style.width = "0";
-};
-
-// 검색창 열기 함수
-const openSearchBox = () => {
-  let inputArea = document.getElementById("input-area");
-  if (inputArea.style.display === "none") {
-    // 기본값을 none으로 설정
-    inputArea.style.display = "inline"; // 클릭했을때 검색창 나타남
-  } else {
-    inputArea.style.display = "none"; // 한번 더 클릭하면 검색창 사라짐
-  }
-};
-
-// 뉴스창 생성 함수
+// 뉴스창 생성
 const render = () => {
   let newsHTML = newsList
     .map(
       (news) => `<div class="row news">
     <div class="col-lg-4 left">
-        <img class="news-image-size" src=${news.urlToImage} 
+        <img class="news-image-size" src=${news.urlToImage || defaultImage} 
         alt=${news.title} 
-        onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU'; this.onerror=null;"/>
+        onerror="this.onerror=null; this.src='${defaultImage}'; "/>
     </div>
     <div class="col-lg-8 right">
         <h2>${news.title}</h2>
         <p>
-        ${news.description == null || news.description == ""
+        ${
+          news.description == null || news.description == ""
             ? "내용없음"
-            : news.description.length >= 200
-            ? news.description.substring(0, 200) + "..." // slice()와 가능 같음
+            : news.description.length >= 100
+            ? news.description.substring(0, 100) + "..."
             : news.description
         }
         </p>
@@ -81,23 +68,71 @@ const render = () => {
   document.getElementById("news-board").innerHTML = newsHTML;
 };
 
-// 카테고리별 검색함수
+// 에러 함수
+const errorRender = (errorMessage) => {
+  const errorHTML = `<div class="alert alert-danger" role="alert">
+    ${errorMessage}
+    </div>`;
+
+  document.getElementById("news-board").innerHTML = errorHTML;
+};
+
+// 카테고리별 검색
+menus.forEach((menu) =>
+  menu.addEventListener("click", (event) => getNewsByCategory(event))
+);
 const getNewsByCategory = async (event) => {
-  const category = event.target.textContent.toLowerCase(); // 소문자로 변경
+  const category = event.target.textContent.toLowerCase();
   console.log("category", category);
   url = new URL(
     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}`
   );
-  getNews();
+  await getNews(); // 리펙토링
 };
 
-// 키워드별 검색 함수
+// 키워드별 검색
+searchButton.addEventListener("click", () => {
+  if (searchInput.value === "") {
+    alert("키워드를 입력하세요!");
+  }
+});
 const getNewsByKeyword = async () => {
   const keyword = searchInput.value;
   console.log("keyword", keyword);
   url = new URL(
     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&q=${keyword}`
   );
+  await getNews(); // 리펙토링
+};
+
+// 엔터키 입력
+searchInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter" && searchInput.value !== "") {
+    getNewsByKeyword(event);
+  }
+});
+
+// 클릭하면 인풋창 비워짐
+searchInput.addEventListener("focus", () => {
   searchInput.value = "";
-  getNews();
+});
+
+// 사이드바 열기
+const openNav = () => {
+  document.getElementById("mySidenav").style.width = "200px";
+};
+
+// 사이드바 닫기
+const closeNav = () => {
+  document.getElementById("mySidenav").style.width = "0";
+};
+
+// 검색창 열기
+const openSearchBox = () => {
+  let inputArea = document.getElementById("input-area");
+  if (inputArea.style.display === "none") {
+    inputArea.style.display = "inline";
+  } else {
+    inputArea.style.display = "none";
+  }
 };
