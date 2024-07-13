d32055ae45fa4b0c0a4f7da1bb787eb1a5f1c6d9
Suan Choi <ziki114455@gmail.com>
2024-07-14 01:41:49 +0900

    페이지네이션까지

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+// const API_KEY = '배정받은 키값'
 let newsList = [];
 let url = new URL(
   `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`
 );
-
+let totalResults = 0
+let page = 1
+const pageSize = 10
+const groupSize = 5
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 const menus = document.querySelectorAll(".menus button");
@@ -12,14 +16,20 @@ const defaultImage =
 // 뉴스 API 호출
 const getNews = async () => {
   try {
+    url.searchParams.set("page",page) // &page=page
+    url.searchParams.set("pageSize",pageSize) // url 호출 전에 파라미터 붙임
+
     const response = await fetch(url);
     const data = await response.json();
+    console.log(data)
     if (response.status === 200) {
       if (data.articles.length === 0) {
         throw new Error("뉴스 결과가 없습니다.");
       }
       newsList = data.articles;
+      totalResults = data.totalResults;
       render();
+      paginationRender(); // 뉴스가 나오고 페이지네이션도 같이 나옴
     } else {
       throw new Error(data.message);
     }
@@ -48,7 +58,7 @@ const render = () => {
         onerror="this.onerror=null; this.src='${defaultImage}'; "/>
     </div>
     <div class="col-lg-8 right">
-        <h2>${news.title}</h2>
+        <a class="title" target="_blank" href="${news.url}">${news.title}</a>
         <p>
         ${
           news.description == null || news.description == ""
@@ -88,6 +98,8 @@ const getNewsByCategory = async (event) => {
     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}`
   );
   await getNews(); // 리펙토링
+  moveToPage(1);
+  closeNav();
 };
 
 // 키워드별 검색
@@ -119,7 +131,7 @@ searchInput.addEventListener("focus", () => {
 
 // 사이드바 열기
 const openNav = () => {
-  document.getElementById("mySidenav").style.width = "200px";
+  document.getElementById("mySidenav").style.width = "250px";
 };
 
 // 사이드바 닫기
@@ -136,3 +148,36 @@ const openSearchBox = () => {
     inputArea.style.display = "none";
   }
 };
+
+// 페이지네이션
+const paginationRender = () => {
+    const totalPages = Math.ceil(totalResults / pageSize) // 총 페이지 수
+    const lastGroup = Math.ceil(totalPages / groupSize) // 마지막 페이지 그룹
+    let pageGroup = Math.ceil(page / groupSize) // 현재 페이지가 어떤 그룹에 속해있는지
+    let lastPage = pageGroup * groupSize // 현재 페이지가 속한 그룹의 마지막 페이지
+        if(lastPage > totalPages) { // 페이지수가 5개 이하면 페이지수만큼 보여줌
+            lastPage = totalPages   // 마지막 그룹이 5개 이하면 마지막 페이지 숫자에 맞춰 5개 보여줌
+        }
+    let firstPage = lastPage - (groupSize - 1)<=0?1:lastPage - (groupSize - 1)  // 현재 페이지가 속한 그룹의 첫번째 페이지
+  
+    let paginationHTML = ``
+    if(pageGroup > 1) {
+    paginationHTML += `<li class="page-item" onclick="moveToPage(1)"><a class="page-link" href="#">&lt;&lt;</a>
+    <li class="page-item" onclick="moveToPage(${page-1})"><a class="page-link" href="#">&lt;</a></li>` 
+    }
+    for(let i=firstPage; i<=lastPage; i++) {
+        paginationHTML += `<li class="page-item ${i===page?"active":""}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`
+    }
+    if(pageGroup < lastGroup) {
+    paginationHTML += `<li class="page-item" onclick="moveToPage(${page+1})"><a class="page-link" href="#">&gt;</a></li>
+    <li class="page-item" onclick="moveToPage(${totalPages})"><a class="page-link" href="#">&gt;&gt;</a>`
+    }
+    document.querySelector(".pagination").innerHTML = paginationHTML // 여러개의 class값을 선택하지만 그중 첫번째값만 반환
+}
+
+// 페이지 이동
+const moveToPage = (pageNum) => {
+    console.log("movetopage", pageNum)
+    page = pageNum
+    getNews()
+}
